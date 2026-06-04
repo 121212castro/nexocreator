07297164ba990cc52bf772a60089d85be07161fc
121212castro <121212castro@gmail.com>
2026-06-04 17:39:55 +0200

    Añade hideAll base

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,9 @@
 function $(id) {
   return document.getElementById(id);
 }
+
+function hideAll() {
+  ['home','newMenu','editor','list','preview'].forEach(function(id) {
+    $(id).classList.add('hidden');
+  });
+}
