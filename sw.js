18a4ce03467b4c976e5e71663d5deedd1986d9c8
121212castro <121212castro@gmail.com>
2026-06-05 16:42:26 +0200

    Fix PWA cache updates

--- sw.js
+++ sw.js
@@ -1,4 +1,18 @@
-const VERSION='nexocreator-v2';
-self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',event=>{event.waitUntil(self.clients.claim());});
-self.addEventListener('fetch',()=>{});
+const VERSION='nexocreator-v3';
+
+self.addEventListener('install',event=>{
+ self.skipWaiting();
+});
+
+self.addEventListener('activate',event=>{
+ event.waitUntil((async()=>{
+   const keys=await caches.keys();
+   await Promise.all(keys.filter(k=>k!==VERSION).map(k=>caches.delete(k)));
+   await self.clients.claim();
+ })());
+});
+
+self.addEventListener('fetch',event=>{
+ if(event.request.method!=='GET') return;
+ event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match(event.request)));
+});
